50062aec25a65df956a3bf1607b85eefa66ed4ec
cyj8494 <cyj8494@gmail.com>
2023-10-09 17:46:46 +0900

    2023-10-09

--- src/App.js
+++ src/App.js
@@ -24,23 +24,41 @@ function App() {
                   <img src={vector} alt="vector" />
               </div>
           </div>
+
+          <div className="soundBar">
+
+          </div>
+
           <div className="rightPanel">
               <div className="sidePanelPick">
                   <div className="myTasksButton">My Tasks</div>
                   <div className="myRecordButton">My Records</div>
               </div>
               <div className="dateText">Today: October</div>
-              <div className="todayFocus">
-                  <div className="">
-                      <div className="recordText">Today, you have focused for</div>
-                      <div className="recordDetails"></div>
-                      <div className="recordTime"></div>
-                      <div className="frame">
-                          <div className="tomato"></div>
-                          <div className="multiplier"></div>
-                      </div>
 
+              <div className="container">
+
+                  <div className="focus-box">
+                      <div className="record">
+                          <div className="record-heading">
+                              Today, you have focused for
+                          </div>
+                          <div className="record-details">
+                              <div className="record-time">
+                                  <span className="large-number">50</span>
+                                  Minutes &&nbsp;
+                                  <span className="large-number">23</span>
+                                  Seconds
+                              </div>
+                              <div className="image-box">
+                                  <div className="times-two">2x</div>
+                                  <div className="image"></div>
+                              </div>
+                          </div>
+                      </div>
                   </div>
+
+
               </div>
           </div>
       </div>
